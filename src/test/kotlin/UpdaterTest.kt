393be89aee30b229efe898a865d624fc7e454965
kevin vissers <5878861+kevinvissers@users.noreply.github.com>
2022-08-28 17:03:45 +0200

    Add tests

--- src/test/kotlin/UpdaterTest.kt
+++ src/test/kotlin/UpdaterTest.kt
@@ -1,13 +1,73 @@
 import org.testng.annotations.Test
+import java.io.File
 
 class UpdaterTest {
 
     @Test
     fun tryUpdate() {
-        val updater = Updater(configFileLocation = "test_config.txt", createBackup = true)
+        val updater = Updater(configFileLocation = "test_config.txt", createBackup = true, dryRun = true)
         val result = updater.updateFiles("JohnDoe", "OldPassword".toCharArray(), "NewPassword".toCharArray())
+        println(result)
+
+        assert(!result.success)
+        assert(result.message == "")
+
+        println("test_files/one_password.txt")
+        assert(result.status[0].success)
+        assert(result.status[0].reason == "")
+        assert(result.status[0].fileName == "test_files/one_password.txt")
+
+        println("test_files/multiple_passwords.xml")
+        assert(result.status[1].success)
+        assert(result.status[1].reason == "")
+        assert(result.status[1].fileName == "test_files/multiple_passwords.xml")
+
+        println("test_files/no_password.txt")
+        assert(!result.status[2].success)
+        assert(result.status[2].reason == "Old password not found in file")
+        assert(result.status[2].fileName == "test_files/no_password.txt")
+
+        println("test_files/base64_password")
+        assert(result.status[3].success)
+        assert(result.status[3].reason == "")
+        assert(result.status[3].fileName == "test_files/base64_password")
+
+        println("test_files/base64_normal_password.txt")
+        assert(result.status[4].success)
+        assert(result.status[4].reason == "")
+        assert(result.status[4].fileName == "test_files/base64_normal_password.txt")
+    }
 
 
+    @Test
+    fun update() {
+        val updater = Updater(configFileLocation = "test_config.txt", createBackup = true, dryRun = false)
+        val result = updater.updateFiles("JohnDoe", "OldPassword".toCharArray(), "NewPassword".toCharArray())
+        println(result)
 
+
+        val files = listOf(
+            "test_files/one_password.txt",
+            "test_files/multiple_passwords.xml",
+            "test_files/no_password.txt",
+            "test_files/base64_password",
+            "test_files/base64_normal_password.txt"
+        )
+
+        files.forEach {
+            with(File(it)) updated@ {
+                val updatedText = this@updated.readText()
+
+                with(File("$it.updated")) {
+                    val expectedText = this.readText()
+
+                    println("$it - $it.updated")
+                    println("updateText: $updatedText")
+                    println(System.lineSeparator())
+                    println("expected text: $expectedText")
+                    assert(expectedText == updatedText)
+                }
+            }
+        }
     }
 }
